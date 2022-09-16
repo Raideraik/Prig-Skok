88be586c696e68ae7e23cadd695cec347034ac9c
Raideraik <denkamartinenko@gmail.com>
2022-09-17 00:39:59 +0300

    up

--- Assets/Scripts/BallTracker.cs
+++ Assets/Scripts/BallTracker.cs
@@ -7,6 +7,11 @@ public class BallTracker : MonoBehaviour
     [SerializeField] private GameObject _ball;
     [SerializeField] private float _speed;
 
+    private void Start()
+    {
+        Application.targetFrameRate = 120;
+
+    }
     private void FixedUpdate()
     {
         Vector3 targetPosition = new Vector3(transform.position.x, _ball.transform.position.y, transform.position.z);
